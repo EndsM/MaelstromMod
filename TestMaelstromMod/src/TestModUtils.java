caeeb4faea4ead5f3bd88ffa19b5374f7681483d
EndsM <lonhongsy@gmail.com>
2025-06-10 19:40:07 +0800

    Update TestModUtils.java
    Format it

--- TestMaelstromMod/src/TestModUtils.java
+++ TestMaelstromMod/src/TestModUtils.java
@@ -1,4 +1,3 @@
-
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import org.junit.jupiter.api.Test;
@@ -7,16 +6,14 @@ import com.barribob.MaelstromMod.util.ModUtils;
 
 import net.minecraft.util.math.Vec3d;
 
-class TestModUtils
-{
+class TestModUtils {
     @Test
-    void testLineCallback()
-    {
-	Vec3d[] expected = { new Vec3d(0, 0, 0), new Vec3d(0, 1.5, 0), new Vec3d(0, 3, 0) };
-	Vec3d[] actual = new Vec3d[3];
-	ModUtils.lineCallback(new Vec3d(0, 0, 0), new Vec3d(0, 3, 0), 3, (vec, i) -> {
-	    actual[i] = vec;
-	});
-	assertArrayEquals(expected, actual);
+    void testLineCallback() {
+        Vec3d[] expected = {new Vec3d(0, 0, 0), new Vec3d(0, 1.5, 0), new Vec3d(0, 3, 0)};
+        Vec3d[] actual = new Vec3d[3];
+        ModUtils.lineCallback(new Vec3d(0, 0, 0), new Vec3d(0, 3, 0), 3, (vec, i) -> {
+            actual[i] = vec;
+        });
+        assertArrayEquals(expected, actual);
     }
 }
